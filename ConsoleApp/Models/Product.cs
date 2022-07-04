73a0b3370317a5b5d9820094dd7270b9a72f19da
Paweł Łęczycki <pawel@msd.info.pl>
2022-07-04 10:56:45 +0200

    Property

---
+++ ConsoleApp/Models/Product.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Models
+{
+    public class Product
+    {
+        //backfield - pole zapasowe dla property
+        private string _name;
+
+        //Auto-property
+        public float Price { get; set; } = 10;
+
+        //Full-property
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                var upperValue = value.ToUpper(); //value - nazwa parametru settera
+                _name = upperValue;
+                Modified = DateTime.Now;
+            }
+        }
+
+        //Read-only property
+        //brak możliwości zmiany wartości
+        public DateTime Created { get; } //= DateTime.Now; //inicjalizacja wartości podczas tworzenia obiektu
+        //property z publicznym dostępem do odczytu, ale ograniczonym dostępen do zapisu
+        public DateTime Modified { get; private set; }
+
+        public Product()
+        {
+            Created = DateTime.Now; // inicjalizacja właściwości read-only
+        }
+
+        public void RisePrice(float value)
+        {
+            Price += value;
+        }
+
+        //Property (read-only) + wartość generowana przy dostępie (nie jest składowana/przechowywana)
+        public string Info => $"{Name} ({Price})";
+
+
+        public string PublicField = "I am public!";
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp.Models;
+using Newtonsoft.Json;
+
+Console.WriteLine("Hello!");
+
+
+var product = new Product();
+
+//product.Price = product.Price + product.Price * 0.1f;
+//product.Price += product.Price * 0.1f;
+
+product.RisePrice(10);
+
+Console.WriteLine(product.Modified);
+
+Console.WriteLine(product.Info);
+
+Console.WriteLine(JsonConvert.SerializeObject(product));
