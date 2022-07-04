58c5b252872a4f2ca806540b9491de05fa7179b5
Paweł Łęczycki <pawel@msd.info.pl>
2022-07-04 11:51:06 +0200

    rezerwacja miejsca dla stringa

--- PeopleApp/Program.cs
+++ PeopleApp/Program.cs
@@ -12,7 +12,10 @@ do
     var people = peopleService.GetPeople();
     foreach (var person in people)
     {
-        var line = $"{person.Id}\t{person.LastName}\t{person.FirstName}\t{person.Age}";
+        //var line = person.Id + "\t" + person.LastName + " " + person.FirstName + person.Age;
+        //var line = string.Format("{0} {2} {1} {3}", person.Id, person.FirstName, person.LastName, person.Age);
+
+        var line = $"{person.Id,-3}{person.LastName,-15}{person.FirstName,-10}{person.Age,-3}";
         Console.WriteLine(line);
     }
 
