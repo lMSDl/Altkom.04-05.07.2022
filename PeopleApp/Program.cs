f10e0f65114420431e87e5614ad0abed2e160c8d
Paweł Łęczycki <pawel@msd.info.pl>
2022-07-04 11:36:04 +0200

    PeopleApp

---
+++ PeopleApp/Program.cs
@@ -0,0 +1,78 @@
+﻿using Services;
+using Models;
+
+IPeopleService peopleService = new PeopleService();
+
+bool exit = false;
+
+do
+{
+    Console.Clear();
+
+    var people = peopleService.GetPeople();
+    foreach (var person in people)
+    {
+        var line = $"{person.Id}\t{person.LastName}\t{person.FirstName}\t{person.Age}";
+        Console.WriteLine(line);
+    }
+
+    ShowMenu();
+    var input = PeopleService.GetData("Wybierz co chcesz zrobić:");
+
+    switch (input)
+    {
+        case "1":
+        case "dodaj":
+        case "add":
+            peopleService.Create();
+            break;
+        case "2":
+            {
+            var id = AskForId();
+            peopleService.Update(id);
+            }
+            break;
+        case "3":
+            {
+            var id = AskForId();
+            peopleService.Delete(id);
+            }
+            break;
+        case "4":
+            exit = true;
+            break;
+        default:
+            Console.WriteLine("Błędna opcja");
+            break;
+    }
+} while (!exit);
+
+void ShowMenu()
+{
+    Console.WriteLine("1. Dodaj");
+    Console.WriteLine("2. Modyfikuj");
+    Console.WriteLine("3. Usuń");
+    Console.WriteLine("4. Koniec");
+}
+
+int AskForId()
+{
+    Console.WriteLine("Podaj id:");
+    var input = Console.ReadLine();
+
+    try
+    {
+        var id = int.Parse(input);
+        return id;
+    }
+    catch (FormatException ex)
+    {
+        Console.WriteLine(ex.Message);
+        return AskForId();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+        return 0;
+    }
+}
---
+++ Services/PeopleService.cs
@@ -0,0 +1,100 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    //klasa implementuje interfejs IPeopleService
+    public class PeopleService : IPeopleService
+    {
+        private IList<Person> people;
+
+        public PeopleService()
+        {
+            people = new List<Person>();
+        }
+
+
+        public void Create()
+        {
+            var person = new Person();
+            EditPerson(person);
+
+            person.Id = GenerateId();
+
+            people.Add(person);
+        }
+
+
+        public void Delete(int id)
+        {
+            for (int i = 0; i < people.Count; i++)
+            {
+                var person = people[i];
+                if (person.Id == id)
+                {
+                    people.RemoveAt(i);
+                    break;
+                }
+            }
+        }
+
+        public IEnumerable<Person> GetPeople()
+        {
+            return people;
+        }
+
+        public Person GetPerson(int id)
+        {
+            foreach (var person in people)
+            {
+                if (person.Id == id)
+                    return person;
+            }
+
+            return null;
+        }
+
+        public void Update(int id)
+        {
+            var person = GetPerson(id);
+            if (person != null)
+                EditPerson(person);
+        }
+
+        private int GenerateId()
+        {
+            int id = 0;
+            foreach(var person in people)
+            {
+                id = Math.Max(id, person.Id);
+            }
+
+            return id + 1;
+        }
+
+        public static string GetData(string label)
+        {
+            Console.WriteLine(label);
+            return Console.ReadLine();
+        }
+        private void EditPerson(Person person)
+        {
+            person.FirstName = GetData("Imię:");
+            person.LastName = GetData("Nazwisko:");
+
+            var success = false;
+            int age = 0;
+            while (!success)
+            {
+                var ageString = GetData("Wiek:");
+                //metoda zwraca 2 rezultaty. Pierwszy poprzez return, a drugi przez parametr wyjściowy (out)
+                success = int.TryParse(ageString, out age);
+            }
+            person.Age = age;
+        }
+    }
+}
